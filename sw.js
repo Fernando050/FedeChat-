26267bfa87f57dba34ccafdd2f3ccbfc1c88e3ea
Fernando050 <140735183+Fernando050@users.noreply.github.com>
2026-07-09 00:16:47 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,12 @@
-const CACHE_NAME = 'fedchat-v2'; // Mwen ogmante vèsyon an pou l pran chanjman yo
+const CACHE_NAME = 'fedchat-v1';
 const assets = [
   './',
   './index.html',
   './style.css',
   './script.js',
-  './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Sedgwick+Ave+Display&family=Poppins:wght@300;400;600&display=swap',
-  'https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js',
-  'https://www.gstatic.com/firebasejs/9.22.1/firebase-auth-compat.js',
-  'https://www.gstatic.com/firebasejs/9.22.1/firebase-database-compat.js'
+  './manifest.json'
 ];
 
-// Enstalasyon: Sere tout fichye yo
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -20,18 +15,6 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// Aktivasyon: Netwaye ansyen kach (Cache)
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
-});
-
-// Fetch: Sèvi ak sa ki nan kach la dabò, si pa genyen, chèche sou entènèt
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
