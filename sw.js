3642e207feda73c7eb788ad322b2fc87f6dc1df3
Fernando050 <140735183+Fernando050@users.noreply.github.com>
2026-07-09 15:13:21 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,38 @@
-const CACHE_NAME = 'fedchat-v2.0';
-const assets = [
+const CACHE_NAME = 'fedchat-v1';
+const ASSETS = [
   './',
   './index.html',
   './style.css',
-  './script.js',
-  './manifest.json'
+  './script.js'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
+// Enstale Service Worker la epi sove fichye yo nan kach (Cache)
+self.addEventListener('install', (e) => {
+  e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(assets);
+      return cache.addAll(ASSETS);
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+// Netwaye ansyen kach si gen yon nouvo vèsyon
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
+  );
+});
+
+// Repon ak fichye ki nan kach la pou app a ka rapid
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
